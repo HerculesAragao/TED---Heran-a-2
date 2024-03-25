c036f432ac021966c654ce36c57686448d683fa9
Hércules <126791549+HerculesAragao@users.noreply.github.com>
2024-03-25 10:03:56 -0300

    Add files via upload

---
+++ src/tech/fhaf/contas/ContaEspecial.java
@@ -0,0 +1,13 @@
+package tech.fhaf.contas;
+
+public class ContaEspecial extends Conta{
+    public double limite;
+    public boolean sacar(double valor) {
+        if(this.getSaldo() >= valor) {
+            this.saldo = this.saldo - (valor * 0.99);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
